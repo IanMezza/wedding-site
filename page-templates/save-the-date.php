451f0b13152f876f6d32daa6707bd105e8525239
ianmeza <ian.mezza@gmail.com>
2022-11-23 12:54:56 -0600

    Agrega save the date

---
+++ page-templates/save-the-date.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Template Name: Save the date
+ *
+ * The template for displaying save the date page.
+ *
+ * @package Jack_&_Rose
+ */
+
+get_header('no-menu'); ?>
+
+	<div id="primary" class="content-area no-sidebar">
+		<main id="main" class="site-main" role="main">
+			<h1 class="align-center">Arely & Ian</h1>
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
+
+			<?php endwhile; // End of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer('raw'); ?>
